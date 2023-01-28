9f7f31367d0a426580bb7d48eccafcdcc831c060
sagepages <anthonypages009@gmail.com>
2023-01-28 16:10:08 -0500

    Update Error handling

--- src/app.js
+++ src/app.js
@@ -3,20 +3,31 @@ const cors = require('cors')
 const router = require('./routes/v1')
 const httpStatus = require('http-status')
 const ApiError = require('./errors/ApiError')
+const { errorConverter, errorHandler } = require('./errors/error')
 
 
 const app = express()
 
+// Parse Json request body 
+app.use(express.json())
+
+// Parse urlencoded request body
+app.use(express.urlencoded({ extended: true}));
+
+
 // app.use(cors)
 // app.options('*', cors())
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}));
-
+// v1 api routes
 app.use('/v1', router)
 
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'))
 })
+
+// Convert errors to ApiError and handle 
+app.use(errorConverter)
+app.use(errorHandler)
+
 
 module.exports = app
