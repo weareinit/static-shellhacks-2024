0cb4a132fd32298d160d8aabc137f54b6dc18b75
sagepages <anthonypages009@gmail.com>
2023-01-28 03:42:47 -0500

    Base routing and folder structure

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const router = require('./routes/v1')
+const httpStatus = require('http-status')
+const ApiError = require('./errors/ApiError')
+
+
+const app = express()
+
+// app.use(cors)
+// app.options('*', cors())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/v1', router)
+
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'))
+})
+
+// Abstract server start and database connection to another file
+app.listen(3000, () => {
+  console.log("listening on port 3000")
+})
+
+module.exports = app
