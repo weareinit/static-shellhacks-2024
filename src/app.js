1cecbbeb0b49fefce1de87e26319bb70c566d4c2
sagepages <anthonypages009@gmail.com>
2023-01-30 18:36:23 -0500

    Added http security, rate limiting, and data sanitization

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ const cors = require('cors')
 const router = require('./routes/v1')
 const httpStatus = require('http-status')
 const morgan = require('./config/morgan')
+const helmet = require('helmet')
+const xssClean = require('xss-clean')
 const ApiError = require('./errors/ApiError')
 const { errorConverter, errorHandler } = require('./errors/error')
+const rateLimiter = require('./middleware/ratelimiter')
 
 
 const app = express()
@@ -15,15 +18,25 @@ if(config.env !== 'test'){
   app.use(morgan.successHandler);
 }
 
+// Secure http headers
+app.use(helmet())
+
+// Santize input data
+app.use(xssClean())
+
 // Parse Json request body 
 app.use(express.json())
 
 // Parse urlencoded request body
 app.use(express.urlencoded({ extended: true}));
 
-
+// Enable cors 
 // app.use(cors)
 // app.options('*', cors())
+
+if(config.env === 'production'){
+  app.use('/v1', rateLimiter);
+}
 
 // v1 api routes
 app.use('/v1', router)
