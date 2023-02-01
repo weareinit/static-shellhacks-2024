96bb7cc7862c1637b4d5ca1b1b90846d4d3665f7
sagepages <anthonypages009@gmail.com>
2023-02-01 13:21:36 -0500

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const app = require('./app')
 const config = require('./config/config')
 const logger = require('./config/logger')
 
+let server;
 // Load Database and upon successful connection, Start server
-const server = app.listen(config.port, () => {
+server = app.listen(config.port, () => {
   logger.info(`Listening on port ${config.port}`);
 })
 
