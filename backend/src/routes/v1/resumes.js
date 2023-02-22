2116ceeccf7c279850a5fb80fb85f01a48ead3de
sagepages <anthonypages009@gmail.com>
2023-02-21 19:52:20 -0500

    Update aws-sdk version and tested via endpoint

--- backend/src/routes/v1/resumes.js
+++ backend/src/routes/v1/resumes.js
@@ -1,14 +1,20 @@
 import express from "express";
-import { prisma, s3 } from "../../index.js";
+import { prisma, s3Client } from "../../index.js";
 import { config } from "../../config/config.js"
-
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 export const router = express.Router();
 
 // FIX 
-router.get('/resumes/:resume_path', async (req, res, next) => {
-  const resume_path_param = req.params.resume_path
-  const params = {Bucket: config.aws_bucket_name, Key: resume_path_param, Expires: 60 * 3}
-  const getURL = s3.getSignedUrl('getObject', params);
-  res.render(getURL)
+router.get('/resumes/:file_name', async (req, res, next) => {
+
+  // Only file currently in the bucket is 
+  // SagePagesFullTime2023.docx.pdf
+
+  const file_name_param = req.params.file_name
+  const bucket_params = {Bucket: config.aws_bucket_name, Key: file_name_param}
+  const command = new GetObjectCommand(bucket_params)
+  const getURL = await getSignedUrl(s3Client, command, {expiresIn: 60 * 3});
+  res.send(getURL)
 })
 
