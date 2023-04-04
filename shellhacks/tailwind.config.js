5f35a134c11d1654bebdfd27c632bef4b5194dc4
David Ulloa <dulloa6310@gmail.com>
2023-04-04 09:59:03 -0400

    Added theme to tailwind.config.js

--- shellhacks/tailwind.config.js
+++ shellhacks/tailwind.config.js
@@ -4,12 +4,21 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      deep_blue: "#4A73AC",
+      blue: "#006887",
+      white: "#FFF",
+    },
+    extend: {
+      rotate: {
+        270: "270deg",
+      },
+    },
   },
   plugins: [],
-}
+};
