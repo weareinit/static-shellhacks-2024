268dbfcb4957652b41e75d259c7cff2d4ce4c9ac
David Ulloa <dulloa6310@gmail.com>
2023-04-04 09:55:31 -0400

    Started Sand and Shoreline Section

--- shellhacks/src/pages/index.tsx
+++ shellhacks/src/pages/index.tsx
@@ -1,35 +1,13 @@
-import { useUser } from "@auth0/nextjs-auth0/client";
+import Button from "@/components/Button";
+import SandSection from "@/components/sections/SandSection";
+import ShorelineSection from "@/components/sections/ShorelineSection";
+import Image from "next/image";
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div> Loading ... </div>;
-  if (error) return <div> {error.message} </div>;
-
-  if (user) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <div>
-          Welcome {user.name}!
-          <br />
-          Your email: {user.email}
-          <br />
-          <a style={{backgroundColor: 'blue'}}href="/api/auth/logout">Logout</a>
-          <br />
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <a style={{ backgroundColor: "grey" }} href="/api/auth/login">
-      Login
-    </a>
+    <main className="bg-sand">
+      <SandSection />
+      <ShorelineSection />
+    </main>
   );
 }
