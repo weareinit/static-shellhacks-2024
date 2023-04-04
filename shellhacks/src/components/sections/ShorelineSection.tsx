bbc66d380a943f261a2e21efdb27726e27d0cdd3
David Ulloa <dulloa6310@gmail.com>
2023-04-04 09:58:05 -0400

    Created section with shoreline and ocean tiles

---
+++ shellhacks/src/components/sections/ShorelineSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ShorelineSection() {
+  return (
+    <section>
+      <div className="">
+        <div className=" bg-shore-line w-screen h-12"></div>
+        <div className=" bg-water w-screen min-h-[30vh]" />
+      </div>
+    </section>
+  );
+}
+
+export default ShorelineSection;
