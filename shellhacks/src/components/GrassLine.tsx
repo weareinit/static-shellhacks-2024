b7003ea3f7dbf098311f0e2c7c362c19a10b0400
David Ulloa <dulloa6310@gmail.com>
2023-04-04 09:58:17 -0400

    Created GrassLine component

---
+++ shellhacks/src/components/GrassLine.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GrassLine() {
+  return (
+    <div className="absolute top-0">
+      <div className="bg-grass-tile w-screen h-10" />
+      <div className="bg-grass w-screen h-10 absolute top-3 z-10" />
+      <div className="bg-grass w-screen h-10 absolute top-4 right-3 z-20" />
+    </div>
+  );
+}
+export default GrassLine;
