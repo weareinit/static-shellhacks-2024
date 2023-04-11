836a6e3000305602ca4b0a7c639f34dd8b1e4edb
David Ulloa <dulloa6310@gmail.com>
2023-04-11 15:47:08 -0400

    Removed extra grass layer

--- shellhacks/src/components/GrassLine.tsx
+++ shellhacks/src/components/GrassLine.tsx
@@ -1,13 +1,12 @@
-import React from "react"
+import React from "react";
 
 function GrassLine() {
   return (
     <section className="col-span-1 hidden md:block h-full">
       <div className="bg-grass-tile bg-right bg-repeat-y h-full relative right-0">
         <div className="bg-grass bg-right relative bg-repeat-y h-full top-0 right-5" />
-        <div className="bg-grass bg-center bg-repeat-y h-full " />
       </div>
     </section>
-  )
+  );
 }
-export default GrassLine
+export default GrassLine;
