4ed78743930849c8bca65b1b75f21148c5b425d2
Jacob Schuster <jschuster8765@gmail.com>
2023-04-11 11:15:44 -0400

    delete class

--- shellhacks/src/components/sections/ShorelineSection.tsx
+++ shellhacks/src/components/sections/ShorelineSection.tsx
@@ -5,7 +5,7 @@ function ShorelineSection() {
     <section className="col-span-3 order-last md:order-first ">
       <div className="grid grid-cols-1 md:grid-cols-4 w-100 h-full">
         <div className="col-span-3 bg-water bg-repeat-round" />
-        <div className="col-span-1 order-first md:order-last bg-shore-line max-width-[40px] bg-repeat-round" />
+        <div className="col-span-1 order-first md:order-last bg-shore-line bg-repeat-round" />
       </div>
     </section>
   )
