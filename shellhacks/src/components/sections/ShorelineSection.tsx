207cb0565ee1f9004cc5e10c715ccb9d9e17cc02
David Ulloa <dulloa6310@gmail.com>
2023-04-11 15:47:19 -0400

    Created row-span responsiveness

--- shellhacks/src/components/sections/ShorelineSection.tsx
+++ shellhacks/src/components/sections/ShorelineSection.tsx
@@ -1,14 +1,14 @@
-import React from "react"
+import React from "react";
 
 function ShorelineSection() {
   return (
     <section className="col-span-3 order-last md:order-first ">
-      <div className="grid grid-cols-1 md:grid-cols-4 w-100 h-full">
-        <div className="col-span-3 bg-water bg-repeat-round" />
+      <div className="grid grid-cols-1 grid-rows-4 md:grid-rows-none md:grid-cols-4 w-100 h-full">
+        <div className="col-span-3 h-full bg-water bg-repeat-round row-span-3 md:row-span-full" />
         <div className="col-span-1 order-first md:order-last bg-shore-line bg-repeat-round" />
       </div>
     </section>
-  )
+  );
 }
 
-export default ShorelineSection
+export default ShorelineSection;
