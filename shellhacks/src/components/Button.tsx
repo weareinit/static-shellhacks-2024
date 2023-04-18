a1f1c5599f02cfd216b13aa9586248ab194a3a4d
David Ulloa <dulloa6310@gmail.com>
2023-04-18 17:24:58 -0400

    Added ShellHacks form open on button click

--- shellhacks/src/components/Button.tsx
+++ shellhacks/src/components/Button.tsx
@@ -8,7 +8,11 @@ interface ButtonProps {
 function Button({ text, className }: ButtonProps) {
   return (
     <button
-      className={`bg-blue text-white text-center px-4 py-1 rounded-full max-w-[16em] ${className}`}
+      onClick={(event) => {
+        event.preventDefault();
+        window.open("https://airtable.com/shrkEhas1KhZ1XpFv");
+      }}
+      className={`bg-blue text-white text-center px-4 py-1 rounded-full max-w-[16em] font-inter ${className}`}
     >
       {text}
     </button>
