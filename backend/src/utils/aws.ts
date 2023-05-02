9d3d4c75af6d3487af7a483e714e65af26f76fc2
Jacob Schuster <jschuster8765@gmail.com>
2023-05-02 01:07:23 -0400

    cleanup aws

--- backend/src/utils/aws.ts
+++ backend/src/utils/aws.ts
@@ -15,22 +15,6 @@ export const generateSignedResumeUrl = async (resumeId: string) => {
   return await getSignedUrl(s3Client, command, { expiresIn: 60 * 3 })
 }
 
-// export async function uploadToS3(fileName: string, fileBuffer: Buffer, contentType: string) {
-//   const params: BucketParams = {
-//     Bucket: process.env.AWS_BUCKET_NAME!,
-//     Key: fileName,
-//     Body: fileBuffer,
-//     ContentType: contentType,
-//   }
-
-//   try {
-//     const response: PutObjectCommandOutput = await s3Client.send(new PutObjectCommand(params))
-//     return response
-//   } catch (error) {
-//     throw Error(`Error uploading object to S3 bucket`)
-//   }
-// }
-
 // export async function doesFileExistInS3(fileName: string) {
 //   const params = { Bucket: process.env.AWS_BUCKET_NAME, Key: fileName }
 //   try {
@@ -38,25 +22,6 @@ export const generateSignedResumeUrl = async (resumeId: string) => {
 //     await s3Client.send(headObjectCmd)
 //   } catch (error) {
 //     throw Error(`File not found in S3 bucket`)
-//   }
-// }
-
-// export async function retrieveFromS3(fileName: string) {
-//   const params = { Bucket: process.env.AWS_BUCKET_NAME, Key: fileName }
-//   try {
-//     // Check if file exists
-//     await doesFileExistInS3(fileName)
-
-//     // If it does, then getSignedUrl
-//     const command: GetObjectCommand = new GetObjectCommand(params)
-//     const signedURL = await getSignedUrl(s3Client, command, {
-//       expiresIn: 60 * 3,
-//     })
-//     return { url: signedURL }
-
-//     // If an error occured
-//   } catch (error) {
-//     throw new Error(`File does not exist on S3 bucket`)
 //   }
 // }
 
