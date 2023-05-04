8ae4309bb110f7efed5a7ebf6bc8aeb1f893921e
Jacob Schuster <jschuster8765@gmail.com>
2023-05-04 15:58:56 -0400

    actually remove user route

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -4,12 +4,11 @@ import { router as EventsRouter } from "./events"
 import { router as ApplicantsRouter } from "./applicants"
 import { router as EmailsRouter } from "./emails"
 import { router as ResumesRouter } from "./resumes"
-import { router as UserRouter } from "./user"
 import { router as AdminRouter } from "./admin"
 
 export const router = express.Router()
 
-const routes = [EventsRouter, ApplicantsRouter, EmailsRouter, ResumesRouter, UserRouter]
+const routes = [EventsRouter, ApplicantsRouter, EmailsRouter, ResumesRouter]
 const adminRoutes = [AdminRouter]
 
 // Load additional routes
