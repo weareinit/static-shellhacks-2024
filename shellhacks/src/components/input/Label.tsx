12cc93dfeacdece38e38524c494e4ceb9ba6e962
David Ulloa <dulloa6310@gmail.com>
2023-05-07 21:08:47 -0400

    Removed unncesary htmlFor prop

--- shellhacks/src/components/input/Label.tsx
+++ shellhacks/src/components/input/Label.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 
 interface LabelPropType {
-  htmlFor: string;
   children?: React.ReactNode;
   className?: string;
   hasInter?: boolean;
 }
 
-function Label({ htmlFor, children, className, hasInter }: LabelPropType) {
+function Label({ children, className, hasInter }: LabelPropType) {
   return (
     <label
-      htmlFor={htmlFor}
       className={`${
         hasInter ? "font-inter" : "font-pixel"
       } text-lg ${className}`}
