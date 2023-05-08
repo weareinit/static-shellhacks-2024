5d23ba1773074d1b4ea642d6b2ef6647101a6f11
David Ulloa <dulloa6310@gmail.com>
2023-05-08 11:06:31 -0400

    Incorporated CheckboxInput into Formik

--- shellhacks/src/components/input/CheckboxInput.tsx
+++ shellhacks/src/components/input/CheckboxInput.tsx
@@ -10,30 +10,20 @@ interface CheckboxInputPropTypes {
   hasInter?: boolean;
 }
 
-function CheckboxInput(props: CheckboxInputPropTypes) {
-  const [field, meta] = useField(props);
-  const [value, setValue] = useState<string>("n");
-
+function CheckboxInput({ label, hasInter, ...props }: CheckboxInputPropTypes) {
+  const [field, meta, helpers] = useField(props);
   return (
     <>
-      <Label
-        htmlFor={props.name}
-        className="w-fit flex flex-col"
-        hasInter={props.hasInter}
-      >
-        {props.label}
-        <input {...field} type="checkbox" className=" sr-only" value={value} />
+      <Label className="w-fit flex flex-col" hasInter={hasInter}>
+        {label}
+        <input {...field} {...props} type="checkbox" className=" sr-only" />
         <div
-          className={`w-5 h-5 ${value === "n" ? "bg-white" : "bg-blue"}`}
+          className={`w-5 h-5 ${meta.value === false ? "bg-white" : "bg-blue"}`}
           onClick={() => {
-            setValue((prev) => {
-              if (prev === "n") {
-                return "y";
-              }
-              return "n";
-            });
+            helpers.setValue(!meta.value);
           }}
         />
+        {meta.touched && meta.error ? <div>{meta.error}</div> : null}
       </Label>
     </>
   );
--- shellhacks/src/components/registration/RegisterForm.tsx
+++ shellhacks/src/components/registration/RegisterForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Formik, Form } from "formik";
+import { Formik, Form, FormikProps } from "formik";
 import * as Yup from "yup";
 
 import { useFormOptionContext } from "@/hooks/FormOptionContext";
@@ -9,16 +9,44 @@ import SelectInput from "../input/SelectInput";
 import CheckboxInput from "../input/CheckboxInput";
 
 function RegisterForm() {
-  const formValidation = Yup.object({
-    firstName: Yup.string().required(),
-    lastName: Yup.string().required(),
-    age: Yup.number().required().min(18).max(114),
-    phoneNumber: Yup.string().required(),
+  interface Values {
+    firstName: string;
+    lastName: string;
+    age: string;
+    phoneNumber: string;
+    email: string;
+    school: string;
+    gradYear: string;
+    levelOfStudy: string;
+    country: string;
+    isInternational: boolean;
+    agreedMLHConduct: boolean;
+    agreedMLHPrivacy: boolean;
+    agreedMLHNews: boolean;
+  }
+
+  const formValidation = Yup.object().shape({
+    firstName: Yup.string().required("First Name is equired"),
+    lastName: Yup.string().required("Last Name is required"),
+    age: Yup.number()
+      .required()
+      .min(18, "You must be at least 18 to compete.")
+      .max(114, "114 is the age of the oldest person on Earth..."),
+    phoneNumber: Yup.string()
+      .matches(
+        /^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$/,
+        "Invalid phone number format"
+      )
+      .required("Phone Number is required"),
     email: Yup.string().email().required(),
     school: Yup.string().required(),
     gradYear: Yup.number().required().min(2024).max(2030),
     levelOfStudy: Yup.string().required(),
     country: Yup.string().required(),
+    isInternational: Yup.boolean(),
+    agreedMLHPrivacy: Yup.boolean().oneOf([true], "Must Be Checked"),
+    agreedMLHConduct: Yup.boolean().oneOf([true], "Must Be Checked"),
+    agreedMLHNews: Yup.boolean(),
   });
 
   const { schools, countries } = useFormOptionContext();
@@ -40,57 +68,82 @@ function RegisterForm() {
 
   return (
     <Formik
-      initialValues={formValidation}
-      onSubmit={() => {
+      validateOnChange
+      validationSchema={formValidation}
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        age: "",
+        phoneNumber: "",
+        email: "",
+        school: "",
+        gradYear: "",
+        levelOfStudy: "",
+        country: "",
+        agreedMLHConduct: false,
+        agreedMLHNews: false,
+        agreedMLHPrivacy: false,
+        isInternational: false,
+      }}
+      onSubmit={(values) => {
+        console.log(values);
         return;
       }}
     >
-      <Form className="grid gap-3 my-2">
-        <TextInput label="First Name" name="firstName" type="text" />
-        <TextInput label="Last Name" name="lastName" type="text" />
-        <TextInput label="Age" name="age" type="number" min={18} max={114} />
-        <TextInput label="Phone Number" name="phoneNumber" type="tel" />
-        <TextInput label="Email" name="email" type="email" />
-        <SelectInput label="School" name="school" options={schools} />
-        <SelectInput
-          label="Country of Residency"
-          name="country"
-          options={countries}
-          defaultValue="United States of America"
-        />
-        <TextInput
-          label="Graduation Year"
-          name="gradYear"
-          type="number"
-          min={2023}
-          max={2033}
-        />
-        <SelectInput
-          label="Level of Study"
-          name="levelOfStudy"
-          options={levelsOfStudy}
-        />
-        <CheckboxInput
-          label="Are you an international student?"
-          name="isInternational"
-        />
-        <h2 className=" font-pixel text-lg font-black underline mt-2">
-          We are currently in the process of partnering with MLH. The following
-          3 checkboxes are for this partnership. If we do not end up partnering
-          with MLH, your information will not be shared.
-        </h2>
-        <CheckboxInput
-          label="I have read and agree to the MLH Code of Conduct. (https://static.mlh.io/docs/mlh-code-of-conduct.pdf)"
-          name="mlhCodeOfConduct"
-          hasInter
-        />
-        <CheckboxInput
-          label="I authorize you to share my application/registration information with Major League Hacking for event administration, ranking, and MLH administration in-line with the MLH Privacy Policy (https://mlh.io/privacy).
+      {(props: FormikProps<Values>) => (
+        <Form className="grid gap-3 my-2">
+          <TextInput label="First Name" name="firstName" type="text" />
+          <TextInput label="Last Name" name="lastName" type="text" />
+          <TextInput label="Age" name="age" type="number" min={18} max={114} />
+          <TextInput label="Phone Number" name="phoneNumber" type="tel" />
+          <TextInput label="Email" name="email" type="email" />
+          <SelectInput label="School" name="school" options={schools} />
+          <SelectInput
+            label="Country of Residency"
+            name="country"
+            options={countries}
+            defaultValue="United States of America"
+          />
+          <TextInput
+            label="Graduation Year"
+            name="gradYear"
+            type="number"
+            min={2023}
+            max={2033}
+          />
+          <SelectInput
+            label="Level of Study"
+            name="levelOfStudy"
+            options={levelsOfStudy}
+          />
+          <CheckboxInput
+            label="Are you an international student?"
+            name="isInternational"
+          />
+          <h2 className=" font-pixel text-lg font-black underline mt-2">
+            We are currently in the process of partnering with MLH. The
+            following 3 checkboxes are for this partnership. If we do not end up
+            partnering with MLH, your information will not be shared.
+          </h2>
+          <CheckboxInput
+            label="I have read and agree to the MLH Code of Conduct. (https://static.mlh.io/docs/mlh-code-of-conduct.pdf)"
+            name="agreedMLHConduct"
+            hasInter
+          />
+          <CheckboxInput
+            label="I authorize you to share my application/registration information with Major League Hacking for event administration, ranking, and MLH administration in-line with the MLH Privacy Policy (https://mlh.io/privacy).
           I further agree to the terms of both the MLH Contest Terms and Conditions (https://github.com/MLH/mlh-policies/blob/main/contest-terms.md) and the MLH Privacy Policy (https://mlh.io/privacy)."
-          name="mlhPrivacy"
-          hasInter
-        />
-      </Form>
+            name="agreedMLHPrivacy"
+            hasInter
+          />
+          <CheckboxInput
+            label="I authorize MLH to send me occasional emails about relevant events, career opportunities, and community announcements."
+            name="agreedMLHNews"
+            hasInter
+          />
+          <button type="submit">Click ME!</button>
+        </Form>
+      )}
     </Formik>
   );
 }
