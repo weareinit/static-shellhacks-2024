440cb12055ff79a8882ecc8c1add49576d2e03cc
David Ulloa <dulloa6310@gmail.com>
2023-05-07 21:09:07 -0400

    Added display of errors from Formik and Yup

--- shellhacks/src/components/input/TextInput.tsx
+++ shellhacks/src/components/input/TextInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, HTMLInputTypeAttribute } from "react";
+import React, { HTMLInputTypeAttribute } from "react";
 
 import { useField } from "formik";
 
@@ -11,31 +11,28 @@ interface TextInputProps {
   placeholder?: string;
   min?: number;
   max?: number;
-  className?: string;
 }
 
-function TextInput(props: TextInputProps) {
+function TextInput({ label, ...props }: TextInputProps) {
   const [field, meta] = useField(props);
-  const [value, setValue] = useState("");
   return (
     <>
-      <Label
-        htmlFor={props.name}
-        className={`w-fit flex flex-col ${props.className}`}
-      >
-        {props.label}
+      <Label className="w-fit flex flex-col">
+        {label}
         <input
           {...field}
-          className=" border-blue border-2 p-1 h-8 font-inter"
-          placeholder={props.placeholder}
-          type={props.type}
-          min={props.min}
-          max={props.max}
-          value={value}
-          onChange={(event) => {
-            setValue(event.target.value);
-          }}
+          {...props}
+          className={`border-blue border-2 p-1 h-8 font-inter focus:ring-2 ${
+            meta.touched && meta.error
+              ? `border-red-600 focus:ring-red-600/50 focus:outline-none`
+              : `focus:ring-blue/50`
+          }`}
         />
+        {meta.touched && meta.error ? (
+          <div className="font-inter text-sm text-red-600 m-0 p-0 font-semibold underline">
+            {meta.error}
+          </div>
+        ) : null}
       </Label>
     </>
   );
