9d1bc3320c8fc98f256dcb2e653960b1e0ba3055
David Ulloa <dulloa6310@gmail.com>
2023-05-09 16:24:41 -0400

    Fixed viewPort final checkBox reposition bug

--- shellhacks/src/components/Modal.tsx
+++ shellhacks/src/components/Modal.tsx
@@ -12,10 +12,10 @@ function Modal({ children, containerClassName, onBgClick }: ModalPropsType) {
     <div
       onClick={onBgClick}
       id="modal-background"
-      className="absolute top-0 left-0 grid content-center justify-center min-w-screen min-h-screen bg-black bg-opacity-20 z-10 backdrop-blur-sm"
+      className="fixed top-0 left-0 grid content-center justify-center w-screen h-screen bg-black bg-opacity-20 z-10 backdrop-blur-sm"
     >
       <aside
-        className={`p-2 bg-white/50 border-blue rounded-md w-screen h-screen z-20 border-2 overflow-scroll ${containerClassName}`}
+        className={`p-2 bg-white/50 border-blue rounded-md w-screen h-screen overflow-scroll z-20 border-2 ${containerClassName}`}
       >
         {children}
       </aside>
