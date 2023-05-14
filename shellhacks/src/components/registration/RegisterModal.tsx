d715f60f924cf8b37395641a53096377da5e1c89
David Ulloa <dulloa6310@gmail.com>
2023-05-14 16:21:38 -0400

    Added container and background styling

--- shellhacks/src/components/registration/RegisterModal.tsx
+++ shellhacks/src/components/registration/RegisterModal.tsx
@@ -1,23 +1,28 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
-import Modal from "../Modal"
-import Button from "../input/Button"
-import RegisterForm from "./RegisterForm"
+import Modal from "../Modal";
+import Button from "../input/Button";
+import RegisterForm from "./RegisterForm";
 
 interface RegisterModalProps {
-  toClose: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  toClose: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 export default function RegisterModal({ toClose }: RegisterModalProps) {
   return (
-    <Modal containerClassName="">
+    <Modal
+      containerClassName="w-screen h-screen z-20"
+      backgroundClassName="z-10"
+    >
       <header className="relative row-start-1 row-span-1 h-fit flex flex-row justify-between">
-        <h1 className=" pt-1 row-start-2 row-span-1 font-pixel underline text-blue text-3xl">Registration</h1>
+        <h1 className=" pt-1 row-start-2 row-span-1 font-pixel underline text-blue text-3xl">
+          Registration
+        </h1>
         <Button className="font-pixel text-xl  text-blue" onClick={toClose}>
           <h1 className="text-base">CLOSE</h1>
         </Button>
       </header>
       <RegisterForm />
     </Modal>
-  )
+  );
 }
