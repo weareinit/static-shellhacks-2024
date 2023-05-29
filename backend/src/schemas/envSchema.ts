8fb00d7ed0bbfb8776ca78e5b69d0cb6a3fcd692
David Ulloa <dulloa6310@gmail.com>
2023-05-29 12:06:32 -0700

    Removed URL requirement for db url

--- backend/src/schemas/envSchema.ts
+++ backend/src/schemas/envSchema.ts
@@ -1,8 +1,8 @@
-import { z } from "zod"
+import { z } from "zod";
 
 export const envSchema = z.object({
   PORT: z.string().default("8000"),
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string(),
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_ACCESS_KEY: z.string(),
   AWS_REGION: z.string(),
@@ -12,4 +12,4 @@ export const envSchema = z.object({
   AUDIENCE: z.string().url(),
   AUTH0_API_V2_TOKEN: z.string(),
   AUTH0_DOMAIN: z.string(),
-})
+});
