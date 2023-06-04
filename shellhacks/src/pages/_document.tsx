47377a5b9d0b28fa79820549347e06eae49314ad
quigongian <gipitz@protonmail.com>
2023-06-04 12:15:49 -0400

    favico changed!

--- shellhacks/src/pages/_document.tsx
+++ shellhacks/src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
