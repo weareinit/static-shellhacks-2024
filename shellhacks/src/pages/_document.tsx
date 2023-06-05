df305feab34e8dba5003f70130d5822ea15b0ac6
styled <smyvensestime@gmail.com>
2023-06-05 13:54:12 -0400

    Changed title to `ShellHacks`

--- shellhacks/src/pages/_document.tsx
+++ shellhacks/src/pages/_document.tsx
@@ -5,9 +5,23 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -18,6 +32,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <title>ShellHacks</title>
       </Head>
       <body>
         <Main />
