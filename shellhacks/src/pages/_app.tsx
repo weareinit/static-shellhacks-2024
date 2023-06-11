48620275cf95c97f5f078964fbed5710a2f464dd
David Ulloa <dulloa6310@gmail.com>
2023-06-11 16:36:23 -0700

    Moved title tag into _app

--- shellhacks/src/pages/_app.tsx
+++ shellhacks/src/pages/_app.tsx
@@ -1,16 +1,19 @@
-import "@/styles/globals.css"
-import type { AppProps } from "next/app"
-import { UserProvider } from "@auth0/nextjs-auth0/client"
-import { QueryClient, QueryClientProvider } from "react-query"
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <Component {...pageProps} />
-      </UserProvider>
-    </QueryClientProvider>
-  )
+    <>
+      <title>ShellHacks</title>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
+      </QueryClientProvider>
+    </>
+  );
 }
--- shellhacks/src/pages/_document.tsx
+++ shellhacks/src/pages/_document.tsx
@@ -32,7 +32,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        <title>ShellHacks</title>
       </Head>
       <body>
         <Main />
