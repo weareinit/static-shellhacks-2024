596737b24e2a5d9cc37224ba3a401154620296a3
David Ulloa <dulloa6310@gmail.com>
2023-07-01 11:55:22 -0700

    Implemented new containers on index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { parseCSV } from "@/util/parseCSV";
+
 import { FormOptionContextProvider } from "@/hooks/FormOptionContext";
 import { ShowRegistrationProvider } from "@/hooks/ShowRegistrationContext";
-import SandSection from "@/components/sections/SandSection";
-import ShorelineSection from "@/components/sections/ShorelineSection";
-import GrassLine from "@/components/GrassLine";
-import Image from "next/image";
+import Shoreline from "@/components/decorations/Shoreline";
+import GrassLine from "@/components/decorations/Grassline";
+import Content from "@/components/sections/Content";
+import MLHBanner from "@/components/decorations/MLHBanner";
+import Decorations from "@/components/decorations/Decorations";
 
 interface HomeProps {
   schools: string[];
@@ -13,23 +15,16 @@ interface HomeProps {
 
 export default function Home({ schools, countries }: HomeProps) {
   return (
-    <main className="bg-sand h-screen grid grid-cols-1 md:grid-cols-9">
-      <div className="w-16 h-28 absolute top-0 left-4 z-1000">
-        <Image
-          src="/assets/mlh-trust-badge-white-2024.png"
-          fill
-          alt="Major League Hacking 2024 Hackathon Season"
-        />
-      </div>
-
-      <ShorelineSection />
+    <div className="bg-sand min-h-screen min-w-screen grid grid-cols-1 md:grid-cols-12">
+      <Decorations />
+      <Shoreline />
+      <GrassLine />
       <ShowRegistrationProvider>
         <FormOptionContextProvider schools={schools} countries={countries}>
-          <SandSection />
+          <Content />
         </FormOptionContextProvider>
       </ShowRegistrationProvider>
-      <GrassLine />
-    </main>
+    </div>
   );
 }
 
@@ -48,9 +43,7 @@ interface CountryDataType {
 }
 
 export async function getStaticProps() {
-  const schoolData: string[] = await parseCSV<string>(
-    "https://raw.githubusercontent.com/MLH/mlh-policies/main/schools.csv"
-  );
+  const schoolData: string[] = await parseCSV<string>("https://raw.githubusercontent.com/MLH/mlh-policies/main/schools.csv");
 
   const schools = schoolData
     .map((school) => {
@@ -58,10 +51,7 @@ export async function getStaticProps() {
     })
     .splice(1);
 
-  const countryData: CountryDataType[] = await parseCSV<CountryDataType>(
-    "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv",
-    true
-  );
+  const countryData: CountryDataType[] = await parseCSV<CountryDataType>("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv", true);
 
   const countries = countryData.map((country) => {
     return country.name;
