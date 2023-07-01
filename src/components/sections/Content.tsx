b5df528b48ad6de72796e461c2b05a4b02c01639
David Ulloa <dulloa6310@gmail.com>
2023-07-01 11:54:02 -0700

    Created Content container

---
+++ src/components/sections/Content.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+
+import { useShowRegistrationContext } from "@/hooks/ShowRegistrationContext";
+import RegisterModal from "../registration/RegisterModal";
+import MLHBanner from "../decorations/MLHBanner";
+import Welcome from "./Welcome";
+import AboutUs from "./AboutUs";
+import BlurBackdrop from "../decorations/BlurBackdrop";
+
+function Content() {
+  const { showRegistration, setShowRegistration, finishedRegistration } = useShowRegistrationContext();
+
+  useEffect(() => {
+    if (showRegistration) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [showRegistration]);
+
+  let modal = <div className="fixed top-0 left-0 backdrop-blur-sm w-screen h-screen"></div>;
+
+  return (
+    // TODO: Implement sky section design, with About Us, Welcome, FAQ
+    <main className="flex flex-col min-h-screen items-center md:col-span-8 md:col-start-3 row-start-1 col-span-1 z-10 pb-24">
+      <MLHBanner />
+      <Welcome />
+      <AboutUs />
+      {showRegistration && (
+        <RegisterModal
+          toClose={(event) => {
+            event.preventDefault();
+            setShowRegistration(false);
+          }}
+        />
+      )}
+      <BlurBackdrop />
+    </main>
+  );
+}
+
+export default Content;
