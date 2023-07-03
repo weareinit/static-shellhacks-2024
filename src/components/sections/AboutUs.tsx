b48d59a4cba0af0f907e83e1b01ceb590db9aa35
David Ulloa <dulloa6310@gmail.com>
2023-07-02 19:52:36 -0700

    Fixed Vincent alt

--- src/components/sections/AboutUs.tsx
+++ src/components/sections/AboutUs.tsx
@@ -9,7 +9,7 @@ function AboutUsImages() {
         <FillImage src="/assets/cup_stack.jpeg" alt="Hackathon participants in last year's shellhacks" className=" w-52 h-52 rounded-pixel" />
       </div>
       <div className="absolute w-fit bottom-0 right-0 hover:z-10">
-        <FillImage src="/assets/init_eboard_mem.jpeg" alt="Vincent Carrancho, a ShellHacks volunteer, holding up a ShellHacks 2022 sign" className="w-52 h-52 rounded-pixel" />
+        <FillImage src="/assets/init_eboard_mem.jpeg" alt="Vincent Carrancho, a ShellHacks organizer, holding up a ShellHacks 2022 sign" className="w-52 h-52 rounded-pixel" />
       </div>
     </div>
   );
