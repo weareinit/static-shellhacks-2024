33d5b86fff6c921df00da77ff0dca59a0c97e2fd
David Ulloa <dulloa6310@gmail.com>
2023-07-09 20:55:58 -0700

    Allow multiple DecorationContainers

--- src/components/decorations/DecorationContainer.tsx
+++ src/components/decorations/DecorationContainer.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 
-function DecorationContainer({ children }: { children: ReactElement[] }) {
-  return <div className="hidden md:grid grid-cols-24 grid-rows-24 col-span-10 row-start-1 col-start-2 h-screen">{children}</div>;
+function DecorationContainer({ children }: { children: ReactElement[] | ReactElement }) {
+  return <div className="hidden md:grid grid-cols-24 grid-rows-24 h-screen w-full">{children}</div>;
 }
 
 export default DecorationContainer;
