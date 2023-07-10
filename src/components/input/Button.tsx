0ec14a5956c5fe19081f2721b76c7ac9d7b02aa5
David Ulloa <dulloa6310@gmail.com>
2023-07-09 20:58:55 -0700

    Removed Button rounding

--- src/components/input/Button.tsx
+++ src/components/input/Button.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function Button({ children, className, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button {...props} className={`text-center py-1 px-3 rounded-sm font-pixel sm:text-lg ${className}`}>
+    <button {...props} className={`text-center py-1 px-3 font-pixel sm:text-lg ${className}`}>
       {children}
     </button>
   );
