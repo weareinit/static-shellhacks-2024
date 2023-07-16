9b83a1db38639da37fe193235cb8fffd5980b023
David Ulloa <dulloa6310@gmail.com>
2023-07-16 11:42:20 -0700

    Renamed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,10 +21,10 @@ module.exports = {
       },
       colors: {
         deep_blue: "#3C72A7",
-        crate_brown: "#6B5E51",
-        navbar_background: "#E5D1B8",
+        dark_brown: "#A1917C",
+        crate_brown: "#695E53",
+        caramel_brown: "#E2D2BB",
         cyan_blue: "#24B2C0",
-        caramel_brown: "#A59079",
         grass_green: "#8EBE6B",
         blue: "#006887",
         pink: "#FF66A4",
