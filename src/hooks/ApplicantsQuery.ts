49695e0b392b0b32935ac69542393f3cdd1bb79d
Jacob Schuster <jschuster8765@gmail.com>
2023-07-16 14:38:58 -0400

    check in

--- src/hooks/ApplicantsQuery.ts
+++ src/hooks/ApplicantsQuery.ts
@@ -5,21 +5,27 @@ import { z } from "zod";
 type ApplicantFilterType = z.infer<typeof applicantFiltersSchema>;
 
 //Note: Right now the name is filtered from the frontend, though it will proably be moved to the backend
-export const useApplicantsQuery = (filters: ApplicantFilterType, name: string) => {
+export const useApplicantsQuery = (filters: ApplicantFilterType, name: string = "") => {
   const getApplicants = async () => {
-    const params = new URLSearchParams(filters as unknown as Record<string, string>).toString();
-    const response = await fetch(`/api/admin/applications?${params}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "applicantion/json",
-      },
-    });
+    try {
+      applicantFiltersSchema.parse(filters);
+      const params = new URLSearchParams(filters as unknown as Record<string, string>).toString();
+      const response = await fetch(`/api/admin/applications?${params}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "applicantion/json",
+        },
+      });
 
-    if (!response.ok) {
-      throw new Error("Error fetching applicant");
+      if (!response.ok) {
+        throw new Error("Error fetching applicant");
+      }
+
+      return response.json();
+    } catch (e) {
+      console.error(e);
+      return [];
     }
-
-    return response.json();
   };
 
   return useQuery(["applicants", filters], getApplicants, {
