bff63960e317ead8c249f45da4c2306fbe767078
David Ulloa <dulloa6310@gmail.com>
2023-07-16 17:52:02 -0700

    Fixed schedule resizing

--- src/components/sections/Schedule.tsx
+++ src/components/sections/Schedule.tsx
@@ -11,7 +11,7 @@ function ScheduleReminder() {
 
 function ScheduleCard({ weekDay, scheduleItems, className }: { weekDay: String; scheduleItems: String[]; className?: String }) {
   return (
-    <div className={`flex flex-col bg-caramel_brown min-w-[200px] h-[250px] justify-self-center lg:min-w-[250px] lg:h-[300px] ${className || ""}`}>
+    <div className={`flex flex-col bg-caramel_brown min-w-[200px] h-[250px] lg:min-w-[250px] lg:h-[300px] ${className}`}>
       <h2 className="font-pixel py-1 text-lg text-center w-full bg-deep_blue text-white">{weekDay}</h2>
       <ul className="p-4">
         {scheduleItems.map((item, index) => (
@@ -28,15 +28,15 @@ function Schedule() {
   return (
     <section className="mt-36">
       <h1 className="mb-12 text-4xl text-crate_brown text-center">Schedule</h1>
-      <div className="grid grid-cols-2 gap-5 lg:grid-cols-3 w-fit justify-items-center items-center justify-center content-center place-items-center">
+      <div className="grid grid-cols-2 gap-5 lg:gap-y-6 lg:grid-cols-3 w-fit justify-items-center items-center justify-center content-center place-items-center">
         <ScheduleCard
-          className="justify-self-start"
+          className=" justify-self-start"
           weekDay="Friday"
           scheduleItems={["4 PM : Check In", "5 PM : Dinner", "6 PM : Opening Ceremony", "7 PM : Sponsor Fair", "8 PM : Hacking Starts", "9 PM : Team Building"]}
         />
         <ScheduleCard weekDay="Saturday" scheduleItems={["4 PM : Check In", "5 PM : Dinner", "6 PM : Opening Ceremony", "7 PM : Sponsor Fair", "8 PM : Hacking Starts", "9 PM : Team Building"]} />
         <ScheduleCard
-          className="justify-self-end"
+          className=" justify-self-end"
           weekDay="Sunday"
           scheduleItems={["4 PM : Check In", "5 PM : Dinner", "6 PM : Opening Ceremony", "7 PM : Sponsor Fair", "8 PM : Hacking Starts", "9 PM : Team Building"]}
         />
