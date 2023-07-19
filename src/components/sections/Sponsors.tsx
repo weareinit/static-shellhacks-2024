61cd99e1ced04c5d72cdb02288e0ee8f5ed11eed
David Ulloa <dulloa6310@gmail.com>
2023-07-19 00:38:28 -0700

    Made responsive for mobile, added sponsor divider

--- src/components/sections/Sponsors.tsx
+++ src/components/sections/Sponsors.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function SponsorImage({ className, src, alt }: { className?: string; src: string; alt: string }) {
   return (
-    <div className={`min-w-[200px] min-h-[150px] w-full relative ${className}`}>
+    <div className={`col-span-2 min-w-[200px] min-h-[150px] w-full relative ${className}`}>
       <Image src={src} alt={alt} className=" object-contain p-5" fill />
     </div>
   );
@@ -10,11 +10,12 @@ function SponsorImage({ className, src, alt }: { className?: string; src: string
 
 function Sponsors() {
   return (
-    <section className=" m-4 p-4 mt-36 min-w-[300px] w-[70vw] max-w-[1200px] flex flex-col justify-center items-center">
+    <section className=" m-4 p-4 mt-36 min-w-[200px] w-[70vw] max-w-[1200px] flex flex-col justify-center items-center">
       <h1 className=" text-4xl text-crate_brown text-center ">Sponsors</h1>
-      <div className="grid grid-cols-2 md:grid-cols-12 gap-5 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-5 w-full">
         <SponsorImage src="/assets/sponsors/INIT FIU.svg" alt="INIT FIU's Logo" className="col-span-2 md:col-span-12" />
         <SponsorImage src="/assets/sponsors/Microsoft.svg" alt="Microsoft Logo" className="col-span-2 md:col-span-12" />
+        <div className="col-span-2 md:col-span-12 min-h-[5px] bg-dark_brown rounded-lg" />
         <SponsorImage src="/assets/sponsors/Xbox.svg" alt="Xbox Logo" className="md:col-span-4" />
         <SponsorImage src="/assets/sponsors/Google.svg" alt="Google Logo" className="md:col-span-4" />
         <SponsorImage src="/assets/sponsors/Waymo.svg" alt="Waymo Logo" className="md:col-span-4" />
