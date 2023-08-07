0decaca3efc2f70d9e2885a02cb4e74526fda96c
Jacob Schuster <jschuster8765@gmail.com>
2023-08-06 21:56:43 -0400

    update accepted template

--- src/util/aws.ts
+++ src/util/aws.ts
@@ -79,7 +79,7 @@ export const sendAcceptanceEmails = async (applicants: EmailPayload[]) => {
   const params: SendBulkTemplatedEmailCommandInput = {
     Destinations: destinations,
     Source: "fiuoperations@weareinit.org",
-    Template: "status-accepted-template-updated",
+    Template: "status-accepted-template-08-06",
     DefaultTemplateData: JSON.stringify({ first_name: "first_name" }),
   };
 
