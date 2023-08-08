bc1d272d2df0205fd1d8833e0bb785647e8c6c52
David Ulloa <dulloa6310@gmail.com>
2023-08-07 19:15:01 -0700

    Added hacker guide into dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -11,6 +11,9 @@ import green from "/public/assets/decorations/green_umbrella.png";
 import Navbar from "@/components/dashboard/Navbar";
 import { application_status_enums } from "@prisma/client";
 import { useAppUpdateMutation } from "@/hooks/ApplicationUpdateMutation";
+import Button from "@/components/input/Button";
+import HackerGuide from "@/components/sections/HackerGuide";
+import { useHackerGuideContext } from "@/hooks/ShowHackerGuideContext";
 
 const getApplicant = async ({ queryKey }: { queryKey: any }) => {
   const [_, email] = queryKey;
@@ -31,6 +34,7 @@ const getApplicant = async ({ queryKey }: { queryKey: any }) => {
 const Dashboard = () => {
   const { user } = useUser();
   const { data, isLoading, error } = useQuery(["applicant", user?.email], getApplicant);
+  const { showHackerGuide, setShowHackerGuide } = useHackerGuideContext();
   const applicantData = data?.applicant;
 
   const appUpdateMutation = useAppUpdateMutation();
@@ -82,6 +86,9 @@ const Dashboard = () => {
     case application_status_enums.withdrawn:
       applicationStatusMessage = "You have withdrawn! :(";
       break;
+    case application_status_enums.waitlisted:
+      applicationStatusMessage = "You are waitlisted";
+      break;
     default:
       applicationStatusMessage = "";
   }
@@ -115,6 +122,7 @@ const Dashboard = () => {
       </div>
 
       <div className="max-w-md mx-auto">
+        {showHackerGuide && <HackerGuide />}
         {applicantData.application_status === application_status_enums.accepted && (
           <div className="mt-4 bg-white rounded-md shadow-md p-6 flex flex-col justify-center">
             <h2 className="text-lg font-medium mb-2">{applicationStatusMessage}</h2>
@@ -133,6 +141,12 @@ const Dashboard = () => {
                 <Image src={decorationImage} alt="Umbrella Decoration" />
               </div>
               <p>{applicationStatusMessage}</p>
+              <Button
+                className="col-span-full w-full bg-deep_blue hover:bg-pink text-white text-center flex items-center justify-center drop-shadow-teal hover:drop-shadow-pink h-[40px] max-w-[250px] m-2 p-4"
+                onClick={() => setShowHackerGuide(true)}
+              >
+                <h2 className="font-console text-sm text-center ">Open Hacker Guide</h2>
+              </Button>
             </div>
           </div>
           <div className="mt-4">
