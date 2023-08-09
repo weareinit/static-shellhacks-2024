50e00cc544420a723516dab7c3aeba3e87eb55b7
quigongian <gipitz@protonmail.com>
2023-08-08 23:31:12 -0400

    organizer fix

--- src/components/sections/Sponsors.tsx
+++ src/components/sections/Sponsors.tsx
@@ -11,7 +11,7 @@ export function SponsorImage({ className, src, alt }: { className?: string; src:
 function Sponsors() {
   return (
     <section className=" m-4 p-4 pt-36 min-w-[200px] w-[70vw] max-w-[1200px] flex flex-col justify-center items-center" id="sponsors">
-      <h1 className=" text-4xl text-crate_brown text-center ">Organizer</h1>
+      <h1 className=" text-4xl text-crate_brown text-center ">Organized By</h1>
       <div className="grid grid-cols-1 md:grid-cols-12 gap-5 w-full">
         <SponsorImage src="/assets/sponsors/INIT_FIU.svg" alt="INIT FIU's Logo" className="col-span-2 md:col-span-12" />
       </div>
