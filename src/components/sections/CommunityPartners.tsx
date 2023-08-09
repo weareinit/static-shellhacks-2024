f9cd58bf531cab0752ece6e1f51f13472fdd71e1
quigongian <gipitz@protonmail.com>
2023-08-08 22:27:58 -0400

    Sponsors update

--- src/components/sections/CommunityPartners.tsx
+++ src/components/sections/CommunityPartners.tsx
@@ -10,10 +10,12 @@ function CommunityPartners() {
         <SponsorImage src="/assets/sponsors/INIT.svg" alt="INIT Logo" className="md:col-span-4" />
         <SponsorImage src="/assets/sponsors/KF.svg" alt="Knight Foundation Logo" className="md:col-span-4" />
         <SponsorImage src="/assets/sponsors/Lab22c.svg" alt="Lab22C Logo" className="md:col-span-4" />
+        <SponsorImage src="/assets/sponsors/Venture_Miami.svg" alt="Venture Miami Logo" className="md:col-span-4" />
         <SponsorImage src="/assets/sponsors/FIU_CEC.svg" alt="FIU College of Engineering and Computing Logo" className="md:col-span-4" />
         <SponsorImage src="/assets/sponsors/FIU_KFSCIS.svg" alt="FIU Knight Foundation School of Computing and Information Sciences Logo" className="md:col-span-4" />
-        <SponsorImage src="/assets/sponsors/MDC.svg" alt="Miami Dade College Logo" className="md:col-span-4" />
         <SponsorImage src="/assets/sponsors/FIU_Honors.svg" alt="FIU Honors College Logo" className="md:col-span-4" />
+        <SponsorImage src="/assets/sponsors/MDC.svg" alt="Miami Dade College Logo" className="md:col-span-4" />
+        <SponsorImage src="/assets/sponsors/MakeItMVP.svg" alt="MakeItMVP Logo" className="md:col-span-4" />
         <SponsorImage src="/assets/sponsors/MLH.svg" alt="Major League Hacking" className="md:col-span-4" />
       </div>
     </section>
