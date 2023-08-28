ccb22c07ed959d801c36ac93da3bd86949e82cb3
Jacob Schuster <jschuster8765@gmail.com>
2023-08-28 00:00:20 -0400

    update email templates

--- src/constants/emailConstants.ts
+++ src/constants/emailConstants.ts
@@ -1,2 +1,2 @@
-export const ACCEPTED_REMINDER_TEMPLATE = "acceptedTemplate18a13fdec68";
-export const ACCEPTED_TEMPLATE = "acceptedTemplate18a13fe6438";
+export const ACCEPTED_REMINDER_TEMPLATE = "acceptedTemplate18a3a4c4ec2";
+export const ACCEPTED_TEMPLATE = "acceptedTemplate18a3a4bdd06";
