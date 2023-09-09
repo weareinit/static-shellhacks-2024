4a4d911a0caa0ab70a46fbd1cecd6a1e90b23b64
David Ulloa <dulloa6310@gmail.com>
2023-09-09 13:35:59 -0400

    Create GUI permission check

--- src/util/auth0Utils.ts
+++ src/util/auth0Utils.ts
@@ -6,3 +6,7 @@ export const isAdmin = async (req: NextApiRequest, res: NextApiResponse) => {
   const isAdmin = session?.user?.["https://shellhacks.net/roles"]?.includes("admin");
   return isAdmin;
 };
+
+export const isGUI = async (req: NextApiRequest, res: NextApiResponse) => {
+  return req.headers.authorization === process.env.GUI_SECRET;
+};
