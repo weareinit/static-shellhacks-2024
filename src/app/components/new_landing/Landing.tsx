ad740f39c363c1eb3d09acf1a0fdaa483195d324
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-05-10 10:16:35 -0400

    landing assets added (sky and ocean)

--- src/app/components/new_landing/Landing.tsx
+++ src/app/components/new_landing/Landing.tsx
@@ -4,10 +4,25 @@ import React from "react";
 import MLHBanner from "../decorations/MLHBanner";
 import LandingContent from "./LandingContent";
 import Logo from "./Logo";
+import Image from "next/image";
 
 const Landing = () => {
   return (
-    <div className="h-screen w-screen bg-gradient-to-b from-[#89ced8] to-[#aad8dc]">
+    <div className="h-screen w-screen">
+      <div className="absolute top-0 -z-20 h-screen w-screen">
+        <Image
+          src="/assets/new/background/beach/Ocean 1.png"
+          alt="beach"
+          fill
+        />
+      </div>
+      <div className="absolute top-[43.5%] -z-10 h-screen w-screen">
+        <Image
+          src="/assets/new/background/beach/Ocean 7.png"
+          alt="beach"
+          fill
+        />
+      </div>
       <MLHBanner />
       <Logo />
       <LandingContent />
