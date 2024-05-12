2a2c91bf68392d3b1ff93c0543c86e505e74d351
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-05-12 08:57:34 -0400

    remove z index (not needed)

--- src/app/components/new_landing/Landing.tsx
+++ src/app/components/new_landing/Landing.tsx
@@ -9,24 +9,24 @@ const Landing = () => {
   return (
     <div className="h-screen w-screen">
       <MLHBanner />
-      <div className="-z-10 relative h-screen w-[100.1vw] right-[1px] bg-[url('/assets/new/background/sky/Sky.svg')]">
+      <div className="relative right-[1px] h-screen w-[100.1vw] bg-[url('/assets/new/background/sky/Sky.svg')]">
         <Logo />
         <LandingContent />
       </div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/beach/Ocean.svg')]"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/beach/Beach.svg')]"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/forest/Forest.svg')]"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/forest/Forest7.svg')]"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/forest/dirt.svg')]"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/forest/dirty.svg')]"></div>
-      <div className="-z-10 h-screen w-screen rotate-180 transform bg-[url('/assets/new/background/forest/dirty.svg')]"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/forest/dirty.svg')]"></div>
-      <div className="bg- -z-10 h-screen w-screen bg-[url('/assets/new/background/lava/Lava6.gif')] bg-no-repeat bg-cover"></div>
-      <div className="relative -z-10 h-screen w-screen bg-[url('/assets/new/background/lava/Lava7.svg')] bg-cover"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/lava/stone.svg')] bg-cover"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/lava/stone.svg')] bg-cover transform rotate-180"></div>
-      <div className="-z-10 h-screen w-screen bg-[url('/assets/new/background/lava/stone.svg')] bg-cover"></div>
-      <div className="-z-10 h-[200px] w-screen bg-[url('/assets/new/background/lava/bedrock.png')] bg-no-repeat"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/beach/Ocean.svg')]"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/beach/Beach.svg')]"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/forest/Forest.svg')]"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/forest/Forest7.svg')]"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/forest/dirt.svg')]"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/forest/dirty.svg')]"></div>
+      <div className="h-screen w-screen rotate-180 transform bg-[url('/assets/new/background/forest/dirty.svg')]"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/forest/dirty.svg')]"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/lava/Lava6.gif')] bg-cover bg-no-repeat"></div>
+      <div className="relative h-screen w-screen bg-[url('/assets/new/background/lava/Lava7.svg')] bg-cover"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/lava/stone.svg')] bg-cover"></div>
+      <div className="h-screen w-screen rotate-180 transform bg-[url('/assets/new/background/lava/stone.svg')] bg-cover"></div>
+      <div className="h-screen w-screen bg-[url('/assets/new/background/lava/stone.svg')] bg-cover"></div>
+      <div className="h-[200px] w-screen bg-[url('/assets/new/background/lava/bedrock.png')] bg-no-repeat"></div>
     </div>
   );
 };
