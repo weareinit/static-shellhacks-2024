7ef85c903bca1b6e5f63a840de6f2c060eb3c76c
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-05-20 23:15:33 -0400

    fix landing page

--- src/app/components/new_landing/LandingButtons.tsx
+++ src/app/components/new_landing/LandingButtons.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 const LandingButtons = () => {
   return (
     <div className=" mt-12 flex flex-col items-center gap-5  text-center ">
-      <div className="m-0 mx-20 w-56 transform rounded-[1.5rem] bg-reddish_grey py-4 text-center text-2xl font-extrabold text-stone-50 no-underline transition-all duration-200 ease-in-out hover:scale-105">
+      <div className="m-0 mx-20 w-56 transform rounded-[1.5rem] bg-reddish_grey pb-2 pt-4 text-center font-zoonaji text-2xl text-stone-50 no-underline transition-all duration-200 ease-in-out hover:scale-105">
         <Link href="/apply">APPLY</Link>
       </div>
-      <div className="m-0 w-96 transform rounded-[1.5rem] bg-stone-50 py-4 text-center text-2xl font-extrabold text-reddish_grey no-underline transition-all duration-200 ease-in-out hover:scale-105">
+      <div className="m-0 w-96 transform rounded-[1.5rem] bg-stone-50 pb-2 pt-4 text-center font-zoonaji text-2xl text-reddish_grey no-underline transition-all duration-200 ease-in-out hover:scale-105">
         <Link href="/dashboard">HACKER DASHBOARD</Link>
       </div>
     </div>
--- src/app/components/new_landing/LandingContent.tsx
+++ src/app/components/new_landing/LandingContent.tsx
@@ -12,11 +12,11 @@ const LandingContent = () => {
   return (
     <>
       <div className="text-center">
-        <p className="text-darker_cyan font-zoonaji text-4xl">
+        <p className="font-zoonaji text-4xl text-darker_cyan">
           The Largest Hackathon in the East Coast!
         </p>
         <div className="flex justify-center">
-          <p className="font-museo mt-1 text-lg">Powered by </p>
+          <p className="mt-1 font-museo text-lg">Powered by </p>
           <div className="relative right-3 top-[0.1625rem] h-8 w-28">
             <Image
               src="/assets/new/logo/Nvidia_Logo.png"
@@ -29,10 +29,10 @@ const LandingContent = () => {
           <div className="mt-5 text-[1.5rem] text-[#000000]">
             September 27th - 29th, 2024
           </div>
-          <div className="text-[1.75rem] text-[#000000]">
-            FIU&apos;s Grahams Center - Miami, FL
+          <div className="text-[1.5rem] text-[#000000]">
+            FIU&apos;s Graham Center - Miami, FL
           </div>
-          <div className="text-ferra mt-1 flex justify-center gap-2 text-2xl">
+          <div className="mt-1 flex justify-center gap-2 text-2xl text-ferra">
             <div className="cursor-pointer">
               <FaDiscord />
             </div>
