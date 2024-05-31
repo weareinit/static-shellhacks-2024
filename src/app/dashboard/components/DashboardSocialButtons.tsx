62ad32f1b2dbe7f2631f50ed2fff28da9ef1a0fb
Jacob Schuster <jschuster8765@gmail.com>
2024-05-31 19:55:33 +0200

    fix alignment

--- src/app/dashboard/components/DashboardSocialButtons.tsx
+++ src/app/dashboard/components/DashboardSocialButtons.tsx
@@ -10,30 +10,30 @@ import { SiDiscord } from "react-icons/si";
 export default function DashboardSocialButtons() {
   return (
     <>
-      <div className="col-span-full flex flex-row justify-center gap-3">
+      <div className="col-span-full flex flex-row gap-3">
         <Link href="https://discord.com/invite/init" target="_blank">
           <SiDiscord
             size={40}
-            className="fill-dark_brown transition ease-in-out hover:scale-125 hover:cursor-pointer hover:fill-pink"
+            className="fill-dark_brown hover:fill-pink transition ease-in-out hover:scale-125 hover:cursor-pointer"
           />
         </Link>
         <Link href="https://www.instagram.com/initofficial/" target="_blank">
           <AiFillInstagram
             size={40}
-            className="fill-dark_brown transition ease-in-out hover:scale-125 hover:cursor-pointer hover:fill-pink"
+            className="fill-dark_brown hover:fill-pink transition ease-in-out hover:scale-125 hover:cursor-pointer"
           />
         </Link>
         <Link href="https://twitter.com/initfiu" target="_blank">
           <AiFillTwitterSquare
             size={40}
-            className="fill-dark_brown transition ease-in-out hover:scale-125 hover:cursor-pointer hover:fill-pink"
+            className="fill-dark_brown hover:fill-pink transition ease-in-out hover:scale-125 hover:cursor-pointer"
           />
         </Link>
 
         <Link href="https://www.facebook.com/init.fiu" target="_blank">
           <AiFillFacebook
             size={40}
-            className="fill-dark_brown transition ease-in-out hover:scale-125 hover:cursor-pointer hover:fill-pink"
+            className="fill-dark_brown hover:fill-pink transition ease-in-out hover:scale-125 hover:cursor-pointer"
           />
         </Link>
         <Link
@@ -42,7 +42,7 @@ export default function DashboardSocialButtons() {
         >
           <AiFillLinkedin
             size={40}
-            className="fill-dark_brown transition ease-in-out hover:scale-125 hover:cursor-pointer hover:fill-pink"
+            className="fill-dark_brown hover:fill-pink transition ease-in-out hover:scale-125 hover:cursor-pointer"
           />
         </Link>
       </div>
