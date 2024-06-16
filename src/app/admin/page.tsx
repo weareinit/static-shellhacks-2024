b0b7d1fa2025abfc7bac694e22f89993333dc1b3
Jacob Schuster <jschuster8765@gmail.com>
2024-06-17 00:24:08 +0300

    fix background

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -37,7 +37,7 @@ const AdminDashboard = async () => {
           </div>
           <div className="relative h-[250px]">
             <Image
-              src="/assets/new/background/sky/sky.png"
+              src="/assets/new/background/sky/Sky.png"
               layout="fill"
               objectFit="cover"
               alt="Sky Continuation"
