a8b42603a691d3231e0f1cada6c9420016f42f1b
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-06-16 15:29:02 -0700

    (possibly) fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import QueryClientComponent from "@/app/state/queryclient";
 import "@/styles/globals.css";
-
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
@@ -8,7 +8,7 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "ShellHacks 2024 ﹣ The Largest Hackathon in Florida!",
   description:
     "Ready to immerse yourself in the ultimate tech experience? Join us for ShellHacks, Florida's Largest Hackathon! Over 1,000 students from across the state and around the world will come together to: Build: Develop innovative projects, Network: Network with top companies, and more! Learn: Learn the latest technologies Develop new skills by attending our technical workshops. Gain experience by building hands-on projects with fellow students. Land your next internship or job by meeting recruiters at our career fair. Win amazing prizes, participate in fun activities, get tons of cool swag, enjoy great food, and much more - all at ShellHacks!",
