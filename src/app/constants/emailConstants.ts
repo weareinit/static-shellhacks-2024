5c1144265e69265b113b6666d806411cf1bd561b
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-07-10 19:09:06 -0700

    attempt at fixing email image

--- src/app/constants/emailConstants.ts
+++ src/app/constants/emailConstants.ts
@@ -1,5 +1,5 @@
 export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder947566";
-export const ACCEPTED_TEMPLATE = "application_accepted_413480";
+export const ACCEPTED_TEMPLATE = "application_accepted_66250";
 // export const STATUS_CONFIRMED_REMINDER_TEMPLATE = "acceptedTemplate18a8b61379b"; //TODO: need to update these...
 // export const STATUS_CONFIRMED_TEMPLATE = "acceptedTemplate18a8b61379b";
 export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_834862";
