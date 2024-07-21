c258424ad480d0a89436d7378c4e510c794c4e3c
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-07-21 10:26:13 -0700

    correct metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   },
   openGraph: {
     type: "website",
-    url: "https://dev.shellhacks.net/",
+    url: "https://shellhacks.net/",
     title: "ShellHacks 2024 ﹣ The Largest Hackathon in Florida!",
     description:
       "Ready to immerse yourself in the ultimate tech experience? Join us for ShellHacks, Florida's Largest Hackathon! Over 1,000 students from across the state and around the world will come together to: Build: Develop innovative projects, Network: Network with top companies, and more! Learn: Learn the latest technologies Develop new skills by attending our technical workshops. Gain experience by building hands-on projects with fellow students. Land your next internship or job by meeting recruiters at our career fair. Win amazing prizes, participate in fun activities, get tons of cool swag, enjoy great food, and much more - all at ShellHacks!",
@@ -25,10 +25,11 @@ export const metadata: Metadata = {
         alt: "ShellHacks 2024 Meta Image",
       },
     ],
+    siteName: "ShellHacks 2024"
   },
   twitter: {
     card: "summary_large_image",
-    url: "https://dev.shellhacks.net/",
+    url: "https://shellhacks.net/",
     title: "ShellHacks 2024 ﹣ The Largest Hackathon in Florida!",
     description:
       "Ready to immerse yourself in the ultimate tech experience? Join us for ShellHacks, Florida's Largest Hackathon! Over 1,000 students from across the state and around the world will come together to: Build: Develop innovative projects, Network: Network with top companies, and more! Learn: Learn the latest technologies Develop new skills by attending our technical workshops. Gain experience by building hands-on projects with fellow students. Land your next internship or job by meeting recruiters at our career fair. Win amazing prizes, participate in fun activities, get tons of cool swag, enjoy great food, and much more - all at ShellHacks!",
