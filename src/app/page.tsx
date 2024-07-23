ec9457e91193bd593ad3a4af6cdbd11c45b660bf
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-07-22 19:35:00 -0700

    twitter tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,11 @@ export const metadata: Metadata = {
         alt: "ShellHacks 2024 Meta Image",
       },
     ],
-    siteName: "ShellHacks 2024"
+    siteName: "ShellHacks 2024",
   },
   twitter: {
     card: "summary_large_image",
-    url: "https://shellhacks.net/",
+    site: "https://shellhacks.net/",
     title: "ShellHacks 2024 ﹣ The Largest Hackathon in Florida!",
     description:
       "Ready to immerse yourself in the ultimate tech experience? Join us for ShellHacks, Florida's Largest Hackathon! Over 1,000 students from across the state and around the world will come together to: Build: Develop innovative projects, Network: Network with top companies, and more! Learn: Learn the latest technologies Develop new skills by attending our technical workshops. Gain experience by building hands-on projects with fellow students. Land your next internship or job by meeting recruiters at our career fair. Win amazing prizes, participate in fun activities, get tons of cool swag, enjoy great food, and much more - all at ShellHacks!",
