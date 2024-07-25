4ed1fa57a0bec0381de456daf2476b8bba5f8a1b
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-07-24 22:43:47 -0700

    add community partners

--- src/app/components/new_landing/new_sections/Sponsors.tsx
+++ src/app/components/new_landing/new_sections/Sponsors.tsx
@@ -23,14 +23,7 @@ const Sponsors = () => {
 
   return (
     <div className="relative">
-      <Image
-        src="/assets/new/background/lava/Stone.png"
-        layout="responsive"
-        width={1789}
-        height={974}
-        alt="Bedrock background"
-        className="relative z-[-10]"
-      />
+      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10]" />
       {/* {Array.from({ length: numImages }, (_, i) => (
         <Image
           key={i}
@@ -41,38 +34,10 @@ const Sponsors = () => {
           className="relative z-[-10]"
         />
       ))} */}
-      <Image
-        src="/assets/new/background/lava/Stone.png"
-        layout="responsive"
-        width={1789}
-        height={974}
-        alt="Bedrock background"
-        className="relative z-[-10]"
-      />
-      <Image
-        src="/assets/new/background/lava/Stone.png"
-        layout="responsive"
-        width={1789}
-        height={974}
-        alt="Bedrock background"
-        className="relative z-[-10] md:hidden"
-      />
-      <Image
-        src="/assets/new/background/lava/Stone.png"
-        layout="responsive"
-        width={1789}
-        height={974}
-        alt="Bedrock background"
-        className="relative z-[-10] sm:hidden"
-      />
-      <Image
-        src="/assets/new/background/lava/Stone.png"
-        layout="responsive"
-        width={1789}
-        height={974}
-        alt="Bedrock background"
-        className="relative z-[-10] xsm:hidden"
-      />
+      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10]" />
+      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] md:hidden" />
+      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] sm:hidden" />
+      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] xsm:hidden" />
       <div className="absolute inset-0 flex flex-col items-center justify-start">
         <h1
           id="sponsors"
@@ -83,65 +48,64 @@ const Sponsors = () => {
 
         <div className="grid auto-rows-auto grid-cols-2 gap-4 xxs:mt-8 sm:mt-0 sm:grid-cols-3">
           <div className="aspect-content aspect-w-1 aspect-h-1">
-            <Image
-              width={500}
-              height={200}
-              alt="microsoft"
-              src="/assets/sponsors/microsoft.svg"
-              className="object-cover"
-            ></Image>
+            <Image width={500} height={200} alt="microsoft" src="/assets/sponsors/microsoft.svg" className="object-cover"></Image>
           </div>
           <div className="aspect-content aspect-w-1 aspect-h-1">
-            <Image
-              width={500}
-              height={200}
-              alt="netflix"
-              src="/assets/sponsors/Netflix 2015 logo.svg"
-              className="object-cover pt-3"
-            ></Image>
+            <Image width={500} height={200} alt="netflix" src="/assets/sponsors/Netflix 2015 logo.svg" className="object-cover pt-3"></Image>
           </div>
           <div className="aspect-content aspect-w-1 aspect-h-1">
-            <Image
-              width={500}
-              height={200}
-              alt="waymo"
-              src="/assets/sponsors/Waymo.svg"
-              className="object-cover"
-            ></Image>
+            <Image width={500} height={200} alt="waymo" src="/assets/sponsors/Waymo.svg" className="object-cover"></Image>
           </div>
           <div className="aspect-content aspect-w-1 aspect-h-1">
-            <Image
-              width={500}
-              height={200}
-              alt="vanguard"
-              src="/assets/sponsors/Vanguard.svg"
-              className="object-cover"
-            ></Image>
+            <Image width={500} height={200} alt="vanguard" src="/assets/sponsors/Vanguard.svg" className="object-cover"></Image>
           </div>
           <div className="aspect-content aspect-w-1 aspect-h-1">
-            <Image
-              width={500}
-              height={200}
-              alt="google"
-              src="/assets/sponsors/Google.svg"
-              className="object-cover"
-            ></Image>
+            <Image width={500} height={200} alt="google" src="/assets/sponsors/Google.svg" className="object-cover"></Image>
           </div>
         </div>
         <p className="m-5 font-museo text-2xl text-white xxs:mt-7 xxs:text-[13px] xsm:text-[15px] sm:text-[15px] md:text-[20px] lg:mt-16 lg:text-[25px] xxl:text-[30px]">
           More sponsors to be revealed soon...
         </p>
+        <h1
+          id="sponsors"
+          className="xss:my-0 mt-32  font-zoonaji text-[60px] text-pastel_orange xxs:text-[25px] xsm:text-[30px] sm:mb-1 sm:text-[40px] md:text-[50px] lg:mb-6 lg:text-[60px] xxl:mb-12 xxl:mt-[100px] xxl:text-[80px]"
+        >
+          Community Partners
+        </h1>
+
+        <div className="grid auto-rows-auto grid-cols-2 gap-4 xxs:mt-8 sm:mt-0 sm:grid-cols-3">
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={500} height={200} alt="microsoft" src="/assets/sponsors/INIT.svg" className="object-cover"></Image>
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={500} height={200} alt="netflix" src="/assets/sponsors/Venture_Miami.svg" className="object-cover pt-3"></Image>
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={500} height={200} alt="waymo" src="/assets/sponsors/MLH.svg" className="object-cover"></Image>
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={500} height={200} alt="vanguard" src="/assets/sponsors/FIU_KFSCIS.svg" className="object-cover"></Image>
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={500} height={200} alt="google" src="/assets/sponsors/Lab22c.svg" className="object-cover"></Image>
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={500} height={200} alt="google" src="/assets/sponsors/FIU_CEC.svg" className="object-cover"></Image>
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={500} height={200} alt="google" src="/assets/sponsors/KnightHacks.svg" className="object-cover"></Image>
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={500} height={200} alt="google" src="/assets/sponsors/Hackabull.svg" className="object-cover"></Image>
+          </div>
+        </div>
+
         <h1 className="xss:my-0 font-zoonaji text-[60px] text-pastel_orange xxs:mt-36 xxs:text-[25px] xsm:text-[30px] sm:mb-1 sm:text-[40px] md:text-[50px] lg:mb-5 lg:mt-96 lg:text-[60px] xxl:mb-5 xxl:mt-[100px] xxl:text-[80px]">
           Want to sponsor us?
         </h1>
         <p className=" font-museo text-2xl text-white xxs:text-[13px] xsm:text-[15px] sm:text-[15px] md:text-[20px]  lg:text-[25px] xxl:text-[30px]">
           Join the roar and reach out to{" "}
-          <a
-            className="underline"
-            href="mailto:init@fiu.edu"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="underline" href="mailto:init@fiu.edu" target="_blank" rel="noopener noreferrer">
             init@fiu.edu
           </a>
           !
