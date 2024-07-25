afa85350c9f9b672a0bb430078fac8b8bf76404b
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-07-24 22:19:19 -0700

    changed accepted template to include shareable image

--- src/app/constants/emailConstants.ts
+++ src/app/constants/emailConstants.ts
@@ -1,5 +1,5 @@
 export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder888364";
-export const ACCEPTED_TEMPLATE = "application_accepted_888364";
+export const ACCEPTED_TEMPLATE = "application_accepted_19984";
 // export const STATUS_CONFIRMED_REMINDER_TEMPLATE = "acceptedTemplate18a8b61379b"; //TODO: need to update these...
 // export const STATUS_CONFIRMED_TEMPLATE = "acceptedTemplate18a8b61379b";
 export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_116564";
