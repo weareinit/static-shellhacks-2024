ef8fbc9fa652ab85501f5dcbb83b5d5cb18ec425
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-07-27 21:41:46 -0400

    removed potential if check to show apply | removed dead code

--- src/app/components/new_landing/LandingButtons.tsx
+++ src/app/components/new_landing/LandingButtons.tsx
@@ -5,26 +5,20 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 
 const LandingButtons = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
 
   const searchParams = useSearchParams();
   const showRegister = searchParams.get("show_register");
 
-  const [isModalOpen, setModalOpen] = useState<boolean>(
-    showRegister === "true",
-  );
+  const [isModalOpen, setModalOpen] = useState<boolean>(showRegister === "true");
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
   return (
     <div className="flex flex-col items-center gap-5 text-center xxs:mt-7 sm:mt-6 md:mt-4 lg:mt-8 xlg:mt-10 xxl:mt-12 xxl:gap-10 ">
-      {(!session || !session.user.hacker_id) && (
+      {!session && (
         <div
-          onClick={
-            session
-              ? openModal
-              : () => signIn("discord", { callbackUrl: "/?show_register=true" })
-          }
+          onClick={session ? openModal : () => signIn("discord", { callbackUrl: "/?show_register=true" })}
           className="m-0 flex transform cursor-pointer items-center justify-center rounded-[1.5rem] 
           bg-reddish_grey pb-2 pt-4 text-center font-zoonaji text-2xl text-stone-50 no-underline transition-all duration-200
            ease-in-out hover:scale-105 xxs:h-[3rem] xxs:w-[8rem] xxs:rounded-[20px] xxs:text-[18px] xsm:h-[4rem] xsm:w-[10rem] xsm:rounded-[25px] xsm:text-[20px] md:h-[3rem] md:w-[8rem] lg:h-[4rem] lg:w-[10rem] lg:text-2xl
