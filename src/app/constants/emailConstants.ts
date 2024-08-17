997eb35925afefcac0542a9039000d9d14e0c599
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-08-17 11:49:10 -0400

    using dev accepted template (merge conflict)

--- src/app/constants/emailConstants.ts
+++ src/app/constants/emailConstants.ts
@@ -1,5 +1,5 @@
 export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder888364";
-export const ACCEPTED_TEMPLATE = "application_accepted_114864";
+export const ACCEPTED_TEMPLATE = "application_accepted_411666";
 // export const STATUS_CONFIRMED_REMINDER_TEMPLATE = "acceptedTemplate18a8b61379b"; //TODO: need to update these...
 // export const STATUS_CONFIRMED_TEMPLATE = "acceptedTemplate18a8b61379b";
 export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_116564";
