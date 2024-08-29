02b4311f4dcd57637d80b9a39dca03c59a4f5322
David Ulloa <41076449+DavidUlloa6310@users.noreply.github.com>
2024-08-29 12:33:08 -0400

    Update emailConstants.ts

--- src/app/constants/emailConstants.ts
+++ src/app/constants/emailConstants.ts
@@ -1,5 +1,5 @@
 export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder_672541";
 export const ACCEPTED_TEMPLATE = "application_accepted_672541";
 // export const STATUS_CONFIRMED_REMINDER_TEMPLATE = "acceptedTemplate18a8b61379b"; //TODO: need to update these...
-// export const STATUS_CONFIRMED_TEMPLATE = "acceptedTemplate18a8b61379b";
+// export const STATUS_CONFIRMED_TEMPLATE = "acceptedTemplate18a8b61379b"
 export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_672541";
