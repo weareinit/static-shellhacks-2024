ae3dafb2670480cc02b9d63457b10f0364da5d24
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-09-05 15:08:32 -0400

    added more sponsors and dealt with layout shift

--- src/app/components/new_landing/new_sections/Sponsors.tsx
+++ src/app/components/new_landing/new_sections/Sponsors.tsx
@@ -11,8 +11,10 @@ const Sponsors = () => {
       <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] xsm:hidden" />
       <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] xsm:hidden" />
       <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] xsm:hidden" />
+      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] xsm:hidden" />
+      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] hidden xsm:block lg:hidden" />
       <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10]" />
-      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] hidden lg:block xl:hidden" />
+      <Image src="/assets/new/background/lava/Stone.png" layout="responsive" width={1789} height={974} alt="Bedrock background" className="relative z-[-10] hidden lg:block min-[1440px]:hidden" />
 
       <div className="absolute inset-0 flex flex-col items-center justify-start">
         <h1
@@ -26,7 +28,7 @@ const Sponsors = () => {
             <TITLE_SPONSOR />
           </div>
         </div>
-        <div className="mx-10 mb-10 grid w-3/5 auto-rows-auto place-items-center xsm:w-5/6 xsm:grid-cols-2 sm:w-4/5">
+        <div className="mx-10 mb-10 grid w-3/5 auto-rows-auto place-items-center xsm:w-5/6 xsm:grid-cols-2 sm:mb-12 sm:w-4/5">
           <TIER_TWO_SPONSORS />
         </div>
         <div className="mx-10 grid auto-rows-auto grid-cols-2 place-items-center gap-4 xxs:mt-8 xsm:grid-cols-3 sm:mt-0">
@@ -56,9 +58,6 @@ const Sponsors = () => {
             <Image width={350} height={200} alt="venture_miami" src="/assets/sponsors/Venture_Miami.svg" className="object-contain pt-3" />
           </div>
           <div className="aspect-content aspect-w-1 aspect-h-1">
-            <Image width={350} height={200} alt="miami_dade_college" src="/assets/sponsors/MDC.svg" className="object-contain pt-3" />
-          </div>
-          <div className="aspect-content aspect-w-1 aspect-h-1">
             <Image width={350} height={200} alt="waymo" src="/assets/sponsors/MLH.svg" className="object-contain" />
           </div>
           <div className="aspect-content aspect-w-1 aspect-h-1">
@@ -75,6 +74,15 @@ const Sponsors = () => {
           </div>
           <div className="aspect-content aspect-w-1 aspect-h-1">
             <Image width={350} height={200} alt="hackabull" src="/assets/sponsors/Hackabull.svg" className="object-contain" />
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={350} height={200} alt="FIU ELT" src="/assets/sponsors/FIU_ELT.svg" className="object-contain" />
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1">
+            <Image width={350} height={200} alt="Knight Foundation" src="/assets/sponsors/KF.svg" className="object-contain" />
+          </div>
+          <div className="aspect-content aspect-w-1 aspect-h-1 col-span-2 w-3/5 place-self-center sm:col-span-3">
+            <Image width={250} height={150} alt="SwampHacks" src="/assets/sponsors/SwampHacks.svg" className="m-auto object-contain text-center" />
           </div>
         </div>
 
@@ -95,6 +103,10 @@ type SponsorType = {
   name: string;
   image: string;
   className?: string;
+  // for Next.js Image Component
+  width?: number;
+  height?: number;
+  last?: boolean;
 };
 
 const TITLE_SPONSOR = () => {
@@ -120,26 +132,35 @@ const TIER_TWO_SPONSORS = () => {
     {
       name: "Netflix",
       image: "/assets/sponsors/Netflix.png",
-      className: "-my-8",
+      className: "-mb-12 -mt-10 scale-75 xsm:scale-90 xsm:-mb-8",
     },
     {
       name: "Vanguard",
       image: "/assets/sponsors/Vanguard.svg",
+      className: "mt-2",
+      width: 400,
+      height: 300,
     },
     {
-      name: "FIU ELT",
-      image: "/assets/sponsors/FIU_ELT.svg",
+      name: "BNY Mellon",
+      image: "/assets/sponsors/BNY_Mellon.svg",
+      className: "my-2 xsm:my-4 xsm:scale-100 sm:scale-125 md:scale-125 lg:scale-[1.75]",
+      width: 115,
+      height: 150,
     },
     {
       name: "Ford",
       image: "/assets/sponsors/Ford.svg",
       className: "mt-2",
+      last: true,
     },
   ];
 
+  const isLastElement = "place-self-center xsm:col-span-2 xsm:w-3/5";
+
   return SPONSORS.map((sponsor) => (
-    <div className={`${sponsor.className}`}>
-      <Image width={500} height={200} alt={sponsor.name} src={sponsor.image} />
+    <div className={`${sponsor.className} ${sponsor.last ? isLastElement : ""}`}>
+      <Image width={sponsor.width ?? 500} height={sponsor.height ?? 200} alt={sponsor.name} src={sponsor.image} className="m-auto text-center" />
     </div>
   ));
 };
@@ -149,20 +170,24 @@ const TIER_THREE_SPONSORS = () => {
     {
       name: "Waymo",
       image: "/assets/sponsors/Waymo.svg",
+      className: "scale-105",
     },
     {
       name: "Google",
       image: "/assets/sponsors/Google.svg",
+      className: "md:scale-105",
     },
     {
       name: "Chevron",
       image: "/assets/sponsors/Chevron.svg",
+      className: "scale-110",
     },
     {
       name: "Patient Safety Technology Challenge",
       image: "/assets/sponsors/PSTC.png",
       width: 100,
       height: 150,
+      className: "lg:scale-150",
     },
     {
       name: "Nvidia",
@@ -179,16 +204,58 @@ const TIER_THREE_SPONSORS = () => {
     {
       name: "State Farm",
       image: "/assets/sponsors/State_Farm.svg",
+      className: "sm:scale-95",
     },
     {
       name: "Capital One",
       image: "/assets/sponsors/Capital_One.svg",
+      className: "sm:scale-125",
+    },
+    {
+      name: "Breakthrough Tech",
+      image: "/assets/sponsors/Breakthrough_Tech.svg",
+      className: "scale-110 sm:scale-125",
+    },
+    {
+      name: "MDC_IT",
+      image: "/assets/sponsors/MDC_IT.svg",
+      width: 60,
+      className: "scale-[0.5] xsm:scale-75 sm:scale-90 md:scale-100 lg:scale-125 lg:mb-2",
+    },
+    {
+      name: "North Star Catering",
+      image: "/assets/sponsors/North_Star_Catering.svg",
+      width: 100,
+      height: 100,
+      className: "md:scale-150 sm:scale-125 lg:scale-[2.0]",
+    },
+    {
+      name: "Snap",
+      image: "/assets/sponsors/Snap.svg",
+      width: 45,
+      height: 100,
+      className: "md:scale-150 sm:scale-125 lg:scale-[1.75]",
+    },
+    {
+      name: "ETH Miami",
+      image: "/assets/sponsors/ETH_Miami.svg",
+      width: 100,
+      height: 100,
+      className: "md:scale-150 sm:scale-150 lg:scale-[2.2]",
+    },
+    {
+      name: "GitHub",
+      image: "/assets/sponsors/GitHub.svg",
+      last: true,
+      className: "lg:mt-2",
     },
   ];
 
+  const isLastElement = "place-self-center col-span-2 xsm:col-auto xsm:w-5/6 w-1/2 sm:w-full lg:w-3/5";
+
   return SPONSORS.map((sponsor) => (
-    <div>
-      <Image width={sponsor.width ?? 400} height={sponsor.height ?? 150} alt={sponsor.name} src={sponsor.image} />
+    <div className={`${sponsor.className} ${sponsor?.last ? isLastElement : ""}`}>
+      <Image width={sponsor.width ?? 400} height={sponsor.height ?? 150} alt={sponsor.name} src={sponsor.image} className="m-auto text-center" />
     </div>
   ));
 };
