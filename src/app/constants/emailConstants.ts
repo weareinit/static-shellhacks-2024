1e68378426ae13e8749ffef29ef9d27acfca9b93
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-09-08 17:14:44 -0400

    update final deadline on email template

--- src/app/constants/emailConstants.ts
+++ src/app/constants/emailConstants.ts
@@ -1,5 +1,5 @@
-export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder927031";
-export const ACCEPTED_TEMPLATE = "application_accepted_927031";
+export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder166519";
+export const ACCEPTED_TEMPLATE = "application_accepted_166519";
 // export const STATUS_CONFIRMED_REMINDER_TEMPLATE = "acceptedTemplate18a8b61379b"; //TODO: need to update these...
 // export const STATUS_CONFIRMED_TEMPLATE = "acceptedTemplate18a8b61379b";
-export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_927031";
+export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_166519";
