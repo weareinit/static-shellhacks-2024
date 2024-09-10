3ce5a07b72832bc4855fda90ee416084d5b2e096
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-09-09 21:14:25 -0400

    update template to include bus information

--- src/app/constants/emailConstants.ts
+++ src/app/constants/emailConstants.ts
@@ -1,5 +1,5 @@
-export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder166519";
-export const ACCEPTED_TEMPLATE = "application_accepted_166519";
+export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder863450";
+export const ACCEPTED_TEMPLATE = "application_accepted_863450";
 // export const STATUS_CONFIRMED_REMINDER_TEMPLATE = "acceptedTemplate18a8b61379b"; //TODO: need to update these...
 // export const STATUS_CONFIRMED_TEMPLATE = "acceptedTemplate18a8b61379b";
-export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_166519";
+export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_863450";
