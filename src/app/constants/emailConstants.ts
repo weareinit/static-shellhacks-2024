900e424b2ec34b3b00214a9ef916bfae15259d9c
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2024-09-17 17:37:03 -0400

    change confirmation date to 19th

--- src/app/constants/emailConstants.ts
+++ src/app/constants/emailConstants.ts
@@ -1,5 +1,5 @@
-export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder186188";
-export const ACCEPTED_TEMPLATE = "application_accepted_186188";
+export const ACCEPTED_REMINDER_TEMPLATE = "application_accepted_reminder851951";
+export const ACCEPTED_TEMPLATE = "application_accepted_851951";
 // export const STATUS_CONFIRMED_REMINDER_TEMPLATE = "acceptedTemplate18a8b61379b"; //TODO: need to update these...
 // export const STATUS_CONFIRMED_TEMPLATE = "acceptedTemplate18a8b61379b";
-export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_186188";
+export const APPLICATION_CONFIRMATION_TEMPLATE = "application_confirmation_851951";
