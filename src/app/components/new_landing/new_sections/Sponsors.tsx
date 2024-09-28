bd1c5a6466e305f71d4edf25055a3df1465788e5
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-09-28 03:40:49 -0400

    i make it pretty (wics logo centered)

--- src/app/components/new_landing/new_sections/Sponsors.tsx
+++ src/app/components/new_landing/new_sections/Sponsors.tsx
@@ -108,14 +108,17 @@ const Sponsors = () => {
           <div className="col-span-1 xsm:scale-[1.3] min-[1100px]:scale-[1.6]">
             <Image width={50} height={100} alt="DSI" src="/assets/sponsors/DSI.svg" />
           </div>
-          <div className="col-span-1 xsm:scale-[1.3] min-[1100px]:mt-6 min-[1100px]:scale-[1.6]">
+          <div className="col-span-1 hidden sm:block xsm:scale-[1.3] min-[1100px]:mt-6 min-[1100px]:scale-[1.6]">
             <Image width={100} height={100} alt="WiCYs" src="/assets/sponsors/WiCYs.png" />
           </div>
-          <div className="xsm:scale-[1.2] min-[1100px]:mt-6">
+          <div className="xsm:scale-[1.2] min-[1100px]:mt-6 relative sm:my-auto sm:left-[9rem] md:left-[11rem] lg:left-[13rem]">
             <Image width={250} height={150} alt="SwampHacks" src="/assets/sponsors/SwampHacks.svg" />
           </div>
-          <div className="xsm:scale-[1.3]  min-[1100px]:mt-6">
+          <div className="xsm:scale-[1.3]  min-[1100px]:mt-6 relative sm:my-auto sm:left-[9rem] md:left-[11rem] lg:left-[13rem]">
             <Image width={100} height={150} alt="Miami Tech Works" src="/assets/sponsors/Miami_Tech_Works.svg" />
+          </div>
+          <div className="col-span-2 sm:hidden xsm:scale-[1.3] min-[1100px]:mt-6 min-[1100px]:scale-[1.6]">
+            <Image width={100} height={100} alt="WiCYs" src="/assets/sponsors/WiCYs.png" />
           </div>
         </div>
 
